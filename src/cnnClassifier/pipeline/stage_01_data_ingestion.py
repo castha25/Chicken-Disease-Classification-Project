53f8b2401075dd37e7d9ff7746ca664d96f47e4c
Astha <castha025@gmail.com>
2026-06-08 21:34:48 +0530

    training added

--- src/cnnClassifier/pipeline/stage_01_data_ingestion.py
+++ src/cnnClassifier/pipeline/stage_01_data_ingestion.py
@@ -11,7 +11,7 @@ class DataIngestionTrainingPipeline:
     
     def main(self):
         config = ConfigurationManager()
-        data_ingestion_config = self.config.get_data_ingestion_config()
+        data_ingestion_config = config.get_data_ingestion_config()
         data_ingestion = DataIngestion(config=data_ingestion_config)
         data_ingestion.download_file()
         data_ingestion.extract_zip_file()
